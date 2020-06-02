106df1eebcb409f260227cd690bed7823efb145c
Awani <awanikendurkar@gmail.com>
2020-06-02 21:34:04 +0530

    Updates

--- task-1/server.py
+++ task-1/server.py
@@ -1,23 +1,3 @@
-################################################################################
-#
-#  Permission is hereby granted, free of charge, to any person obtaining a
-#  copy of this software and associated documentation files (the "Software"),
-#  to deal in the Software without restriction, including without limitation
-#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
-#  and/or sell copies of the Software, and to permit persons to whom the
-#  Software is furnished to do so, subject to the following conditions:
-#
-#  The above copyright notice and this permission notice shall be included in
-#  all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-#  DEALINGS IN THE SOFTWARE.
-
 from itertools import izip
 from random    import normalvariate, random
 from datetime  import timedelta, datetime
@@ -34,12 +14,6 @@ import threading
 from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 from SocketServer   import ThreadingMixIn
 
-################################################################################
-#
-# Config
-
-# Sim params
-
 REALTIME    = True
 SIM_LENGTH  = timedelta(days = 365 * 5)
 MARKET_OPEN = datetime.today().replace(hour = 0, minute = 30, second = 0)
@@ -54,29 +28,20 @@ FREQ = (12,    36,   50)
 
 OVERLAP = 4
 
-################################################################################
-#
 # Test Data
 
 def bwalk(min, max, std):
-    """ Generates a bounded random walk. """
     rng = max - min
     while True:
         max += normalvariate(0, std)
         yield abs((max % (rng * 2)) - rng) + min
 
 def market(t0 = MARKET_OPEN):
-    """ Generates a random series of market conditions,
-        (time, price, spread).
-    """
     for hours, px, spd in izip(bwalk(*FREQ), bwalk(*PX), bwalk(*SPD)):
         yield t0, px, spd
         t0 += timedelta(hours = abs(hours))
 
 def orders(hist):
-    """ Generates a random set of limit orders (time, side, price, size) from
-        a series of market conditions.
-    """
     for t, px, spd in hist:
         stock = 'ABC' if random() > 0.5 else 'DEF'
         side, d  = ('sell', 2) if random() > 0.5 else ('buy', -2)
@@ -85,22 +50,16 @@ def orders(hist):
         yield t, stock, side, order, size
 
 
-################################################################################
 #
 # Order Book
 
 def add_book(book, order, size, _age = 10):
-    """ Add a new order and size to a book, and age the rest of the book. """
     yield order, size, _age
     for o, s, age in book:
         if age > 0:
             yield o, s, age - 1
 
 def clear_order(order, size, book, op = operator.ge, _notional = 0):
-    """ Try to clear a sized order against a book, returning a tuple of
-        (notional, new_book) if successful, and None if not.  _notional is a
-        recursive accumulator and should not be provided by the caller.
-    """
     (top_order, top_size, age), tail = book[0], book[1:]
     if op(order, top_order):
         _notional += min(size, top_size) * top_order
@@ -111,9 +70,6 @@ def clear_order(order, size, book, op = operator.ge, _notional = 0):
             return clear_order(order, -sdiff, tail, op, _notional)
 
 def clear_book(buy = None, sell = None):
-    """ Clears all crossed orders from a buy and sell book, returning the new
-        books uncrossed.
-    """
     while buy and sell:
         order, size, _ = buy[0]
         new_book = clear_order(order, size, sell)
@@ -125,10 +81,6 @@ def clear_book(buy = None, sell = None):
     return buy, sell
 
 def order_book(orders, book, stock_name):
-    """ Generates a series of order books from a series of orders.  Order books
-        are mutable lists, and mutating them during generation will affect the
-        next turn!
-    """
     for t, stock, side, order, size in orders:
         if stock_name == stock:
             new = add_book(book.get(side, []), order, size)
@@ -136,7 +88,6 @@ def order_book(orders, book, stock_name):
         bids, asks = clear_book(**book)
         yield t, bids, asks
 
-################################################################################
 #
 # Test Data Persistence
 
@@ -155,40 +106,28 @@ def read_csv():
         for time, stock, side, order, size in csv.reader(f):
             yield dateutil.parser.parse(time), stock, side, float(order), int(size)
 
-################################################################################
 #
 # Server
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    """ Boilerplate class for a multithreaded HTTP Server, with working
-        shutdown.
-    """
     allow_reuse_address = True
     def shutdown(self):
-        """ Override MRO to shutdown properly. """
         self.socket.close()
         HTTPServer.shutdown(self)
 
 def route(path):
-    """ Decorator for a simple bottle-like web framework.  Routes path to the
-        decorated method, with the rest of the path as an argument.
-    """
     def _route(f):
         setattr(f, '__route__', path)
         return f
     return _route
 
 def read_params(path):
-    """ Read query parameters into a dictionary if they are parseable,
-        otherwise returns None.
-    """
     query = path.split('?')
     if len(query) > 1:
         query = query[1].split('&')
         return dict(map(lambda x: x.split('='), query))
 
 def get(req_handler, routes):
-    """ Map a request to the appropriate route of a routes instance. """
     for name, handler in routes.__class__.__dict__.iteritems():
         if hasattr(handler, "__route__"):
             if None != re.search(handler.__route__, req_handler.path):
@@ -202,9 +141,6 @@ def get(req_handler, routes):
                 return
 
 def run(routes, host = '0.0.0.0', port = 8080):
-    """ Runs a class as a server whose methods have been decorated with
-        @route.
-    """
     class RequestHandler(BaseHTTPRequestHandler):
         def log_message(self, *args, **kwargs):
             pass
@@ -222,7 +158,6 @@ def run(routes, host = '0.0.0.0', port = 8080):
     server.start()
     server.waitForThread()
 
-################################################################################
 #
 # App
 
@@ -232,7 +167,6 @@ ops = {
 }
 
 class App(object):
-    """ The trading game server application. """
 
     def __init__(self):
         self._book_1    = dict()
@@ -268,9 +202,6 @@ class App(object):
 
     @route('/query')
     def handle_query(self, x):
-        """ Takes no arguments, and yields the current top of the book;  the
-            best bid and ask and their sizes
-        """
         try:
             t1, bids1, asks1 = self._current_book_1.next()
             t2, bids2, asks2 = self._current_book_2.next()
@@ -308,7 +239,6 @@ class App(object):
             }
         }]
 
-################################################################################
 #
 # Main
 
